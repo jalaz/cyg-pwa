2bc2ae64212dcd6c6b22ed40dba5a4488edfd53a
jalaz <azaalexey@gmail.com>
2018-10-17 23:44:58 -0400

    add call for routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,51 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import registerWorker from './registerWorker'
+import registerWorker from './registerWorker';
 
-const App = () => (
-    <h2>Hello cyg</h2>
-);
+const API = process.env.CYG_API;
+
+interface IRoute {
+  routeId: string;
+  routeLongName: string;
+}
+
+interface IAppState {
+  routes: IRoute[];
+}
+
+class App extends React.Component<{}, IAppState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      routes: [],
+    };
+  }
+
+  public async componentDidMount() {
+    const routes = await fetch(`${API}/route`).then((r) => r.json());
+    this.setState({
+      routes,
+    });
+  }
+
+  public render() {
+    const { routes } = this.state;
+
+    return (
+      <ul>
+        {routes.map((route) =>
+          <li key={route.routeId}>
+            <a>{route.routeLongName}</a>
+          </li>)}
+      </ul>
+    );
+  }
+}
 
 render(
     <App />,
-    document.getElementById("root")
+    document.getElementById('root'),
 );
 
 registerWorker();
--- webpack.config.js
+++ webpack.config.js
@@ -2,51 +2,63 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 
-module.exports = {
-    entry: {
-        vendor: [
-            'react',
-            'react-dom'
+module.exports = () => {
+    const env = dotenv.config().parsed;
+    console.log(env);
+    const envKeys = Object.keys(env).reduce((prev, next) => {
+        prev[`process.env.${next}`] = JSON.stringify(env[next]);
+        return prev;
+    }, {});
+
+    return {
+        entry: {
+            vendor: [
+                'react',
+                'react-dom'
+            ],
+            app: './src/index.tsx'
+        },
+        module: {
+            rules: [
+                { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+            ]
+        },
+        resolve: {
+            modules: [
+                path.resolve('./src'),
+                path.resolve('./node_modules')
+            ]
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'public'),
+            globalObject: `(typeof self !== 'undefined' ? self : this)`
+        },
+        plugins: [
+            new CleanWebpackPlugin('public'),
+            new CopyWebpackPlugin([
+                {from: './src/assets/icons/*', to: path.resolve(__dirname, 'public'), flatten: true },
+                {from: './src/manifest.json', to: path.resolve(__dirname, 'public') },
+                {from: './src/sw.js', to: path.resolve(__dirname, 'public') },
+            ]),
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, "src/index.html"),
+                filename: "./index.html"
+            }),
+            new webpack.DefinePlugin(envKeys)
         ],
-        app: './src/index.tsx'
-    },
-    module: {
-        rules: [
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-        ]
-    },
-    resolve: {
-        modules: [
-            path.resolve('./src'),
-            path.resolve('./node_modules')
-        ]
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'public'),
-        globalObject: `(typeof self !== 'undefined' ? self : this)`
-    },
-    plugins: [
-        new CleanWebpackPlugin('public'),
-        new CopyWebpackPlugin([
-            {from: './src/assets/icons/*', to: path.resolve(__dirname, 'public'), flatten: true },
-            {from: './src/manifest.json', to: path.resolve(__dirname, 'public') },
-            {from: './src/sw.js', to: path.resolve(__dirname, 'public') },
-        ]),
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, "src/index.html"),
-            filename: "./index.html"
-        })
-    ],
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    name: 'vendor',
-                    chunks: 'initial'
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    vendor: {
+                        name: 'vendor',
+                        chunks: 'initial'
+                    }
                 }
             }
         }
-    }
+    };
 };
